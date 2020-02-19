7e7c923a682c3e35b4894a96ae94dad847a3f9ce
Gower <Gower@GOWRI>
2020-02-19 23:10:33 +0530

    Add project files.

---
+++ LibraryAutomationSystem/Controllers/HomeLASController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryAutomationSystem.Controllers
+{
+    public class HomeLASController : Controller
+    {
+        // GET: HomeLAS
+        public ActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ LibraryAutomationSystem/Controllers/LibraryAutomationSystemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryAutomationSystem.Controllers
+{
+    public class LibraryAutomationSystemController : Controller
+    {
+        // GET: LibraryUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult RegistrationUser()
+        {
+            return View();
+        }
+        public ActionResult LoginUser()
+        {
+            return View();
+        }
+    }
+}
